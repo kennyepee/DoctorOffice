3a0d05b7336f78ad9bb3527952745c7ee74ef65f
Kenny Epee <kennyepee@Kennys-MacBook-Pro.local>
2020-08-31 16:45:39 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String patientName = "Stacey Manson";
+        boolean hasInsurance = false;
+        double temp=102;
+        int numVisits= (int) 5.02;
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: "+patientName);
+        System.out.println("Patient has insurance: "+hasInsurance);
+        System.out.println("Body temprature: " + temp);
+        System.out.println("Number of visits: " + numVisits);
+
+    }
+}
